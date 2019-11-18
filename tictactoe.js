ffea628aa8a8b91d223eefce8b35ce8ab7341637
StarchildTwo <40040986+StarchildTwo@users.noreply.github.com>
2019-11-18 07:32:13 +0100

    some more changes

--- tictactoe.js
+++ tictactoe.js
@@ -1,63 +1,76 @@
 let squares = document.querySelectorAll(".gameSquare");
-let squareArr = Array.from(squares);
+
 let playerTurn = true;
 let won = false;
 // Generella tankar: Gör en function för att kolla "Winning-positions" och gör kollen efter varje drag.
 // 
 let firstPlayer = {
     name: "PlayerOne",
-    won: false,
+    wins: 0,
 
 }
 
 let secPlayer = {
     name: "PlayerTwo",
-    won: false,
+    wins: 0,
 
 }
 
+function play() {
 
-for (let i = 0; i < squares.length; i++) {
-    squares[i].id = i;
-    squares[i].addEventListener("click", function (e) {
-        if (won) return;
-        let square = e.target;
-        if (playerTurn) {
-            playerOne(square);
-        } else playerTwo(square);
+    for (let i = 0; i < squares.length; i++) {
 
-        playerTurn = !playerTurn;
-        checkWinning2();
-    })
-};
+        squares[i].addEventListener("click", function (e) {
+            if (won) return;
+            let square = e.target;
+            if (playerTurn) {
+                playerOne(square);
+
+
+            } else {
+                playerTwo(square);
+
+
+            }
+            console.log(playerTurn);
+
+
+
+            checkWinning2();
+        })
+    };
+}
 
 let startScreen = document.querySelector(".start");
-startScreen.addEventListener("click", function (e) {
+startScreen.addEventListener("click", function () {
     startScreen.classList.toggle("hide");
 });
 
 function playerOne(div) {
     if (div.textContent) return;
-    myTurn = true;
+
     let x = document.createElement("i");
     x.textContent = "clear";
     x.className = "material-icons piece";
 
     div.appendChild(x);
+    playerTurn = !playerTurn;
+
 }
 
 function playerTwo(div) {
     if (div.textContent) return;
     let o = document.createElement("i");
     o.textContent = "radio_button_unchecked";
-    let text = o.textContent;
+
     o.className = "material-icons piece";
     div.appendChild(o);
-    return text;
+    playerTurn = !playerTurn;
+
 }
 
 
-function checkWinning() {
+/* function checkWinning() { // Funkar bra, men ful lösning.
     // check if someone won or tie.
 
     for (let i = 0; i < squares.length; i++) {
@@ -144,33 +157,33 @@ function checkWinning() {
         }
 
     }
-}
+} */
 // Kom på något smart sätt att kolla vinster.
 function checkWinning2() {
-    const winStates = [
-        [0, 1, 2],
+    const winStates = [ // Dessa är helt enkelt alla möjliga vinstrader gjorda i arrayer i en array. 
+        [0, 1, 2], // horisontell rad
         [3, 4, 5],
         [6, 7, 8],
-        [0, 3, 6],
+        [0, 3, 6], // vertikal
         [1, 4, 7],
         [2, 5, 8],
-        [0, 4, 8],
+        [0, 4, 8], // Diagonal
         [2, 4, 6]
     ]
-    for (let i = 0; i < winStates.length; i++) {
-        let player = null;
+    for (let i = 0; i < winStates.length; i++) { // Loop för hela arrayen
+        let player = null; // Null för att hålla reda på första klicket på respektive rad.
         for (let square of winStates[i]) {
-            console.log(square);
-            if (player === null && squares[square].innerText !== "") {
-                player = squares[square].innerText;
-            } else if (player !== squares[square].innerText) {
-                break;
-            } else if (square === winStates[i][winStates[i].length - 1]) {
 
+            if (player === null && squares[square].innerText !== "") { // Om första klicket (alltså null) och rutan inte är tom ->
+                player = squares[square].innerText; // Player = X el O.
+            } else if (player !== squares[square].innerText) { // Om rutan jämte "player"-rutan blir tagen av motståndaren kommer den raden helt enkelt inte räknas med. därav break;
+                break;
+            } else if (square === winStates[i][winStates[i].length - 1]) { // om föregående inte händer kommer det att bli en hel rad när man klickar i sista rutan för respektive vinstrad.
+                // Då görs antagandet att alla rutor i respektive rad är av samma "X" eller "O".
                 won = true;
                 if (player === "clear") {
-                    renderWin(playerOne);
-                } else renderWin(playerTwo);
+                    renderWin(firstPlayer);
+                } else renderWin(secPlayer);
             }
         }
         if (won) break;
@@ -193,8 +206,18 @@ function renderWin(player) {
     resBtn.textContent = "Reset";
     div.appendChild(resBtn);
     document.body.appendChild(div);
-    /* resBtn.addEventListener("click", function () {
+    resBtn.addEventListener("click", function () {
+        div.remove();
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].textContent = "";
+        };
 
-    }) */
+        won = false;
+        playerTurn = true;
+        player.wins++;
+        play();
+        console.log(playerTurn);
+    })
 
 };
+play();
